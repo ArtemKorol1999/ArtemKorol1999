d1740bf5379a323305f30f4cf3b092c9138f1efe
ArtemKorol1999 <artem.korolevskay@gmail.com>
2021-01-26 19:16:59 +0700

    я картошка

--- src/main.kt
+++ src/main.kt
@@ -3,8 +3,5 @@ fun main()
 { var smail = ("# # # # # # # #\n# # # # # # # #\n# O # # # # O #\n# # # # # # # #\n# O # # # # O #\n# # O O O O # #\n# # # # # # # #\n# # # # # # # #")
 print (smail)
 
-
-
-
-
+    
 }
